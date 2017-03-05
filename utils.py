d8399a2b58730f77c2096837886cca8fd424f62f
Ilya Ivanov <ilya.ivanov.se@gmail.com>
2017-03-05 18:39:19 +0300

    utility funtions

---
+++ utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def zero_pad(X, seq_len):
+    return np.array([x[:seq_len - 1] + [0] * max(seq_len - len(x), 1) for x in X])
+
+
+def get_vocabulary_size(X):
+    return max([max(x) for x in X]) + 1  # plus the 0th word
+
+
+def fit_in_vocabulary(X, voc_size):
+    return [[w for w in x if w < voc_size] for x in X]
+
+
+def batch_generator(X, y, batch_size):
+    size = X.shape[0]
+    X_copy = X.copy()
+    y_copy = y.copy()
+    indices = np.arange(size)
+    np.random.shuffle(indices)
+    X_copy = X_copy[indices]
+    y_copy = y_copy[indices]
+    i = 0
+    while True:
+        if i + batch_size <= size:
+            yield X_copy[i:i + batch_size], y_copy[i:i + batch_size]
+            i += batch_size
+        else:
+            i = 0
+            indices = np.arange(size)
+            np.random.shuffle(indices)
+            X_copy = X_copy[indices]
+            y_copy = y_copy[indices]
+            continue
