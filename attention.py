af550e149786c4c13522bd236e42f15374d24492
Ilya Ivanov <ilya.ivanov.se@gmail.com>
2017-04-10 03:51:18 +0300

    supported time major order; detailed docstring

--- attention.py
+++ attention.py
@@ -1,17 +1,55 @@
 import tensorflow as tf
 
 
-def attention(inputs, attention_size):
+def attention(inputs, attention_size, time_major=False):
     """
-    Attention mechanism layer.
+    Attention mechanism layer which reduces RNN/Bi-RNN outputs with Attention vector.
 
-    :param inputs: outputs of RNN/Bi-RNN layer (not final state)
-    :param attention_size: linear size of attention weights
-    :return: outputs of the passed RNN/Bi-RNN reduced with attention vector
+    The idea was proposed in the article "Hierarchical Attention Networks for Document Classification":
+    http://www.aclweb.org/anthology/N16-1174.
+    Args:
+        inputs: The Attention inputs.
+            Matches outputs of RNN/Bi-RNN layer (not final state):
+                In case of RNN, this must be RNN outputs `Tensor`:
+                    If time_major == False (default), this must be a tensor of shape:
+                        `[batch_size, max_time, cell.output_size]`.
+                    If time_major == True, this must be a tensor of shape:
+                        `[max_time, batch_size, cell.output_size]`.
+                In case of Bidirectional RNN, this must be a tuple (outputs_fw, outputs_bw) containing the forward and
+                the backward RNN outputs `Tensor`.
+                    If time_major == False (default),
+                        outputs_fw is a `Tensor` shaped:
+                        `[batch_size, max_time, cell_fw.output_size]`
+                        and outputs_bw is a `Tensor` shaped:
+                        `[batch_size, max_time, cell_bw.output_size]`.
+                    If time_major == True,
+                        outputs_fw is a `Tensor` shaped:
+                        `[max_time, batch_size, cell_fw.output_size]`
+                        and outputs_bw is a `Tensor` shaped:
+                        `[max_time, batch_size, cell_bw.output_size]`.
+        attention_size: Linear size of the Attention weights.
+        time_major: The shape format of the `inputs` Tensors.
+            If true, these `Tensors` must be shaped `[max_time, batch_size, depth]`.
+            If false, these `Tensors` must be shaped `[batch_size, max_time, depth]`.
+            Using `time_major = True` is a bit more efficient because it avoids
+            transposes at the beginning and end of the RNN calculation.  However,
+            most TensorFlow data is batch-major, so by default this function
+            accepts input and emits output in batch-major form.
+    Returns:
+        The Attention output `Tensor`.
+        In case of RNN, this will be a `Tensor` shaped:
+            `[batch_size, cell.output_size]`.
+        In case of Bidirectional RNN, this will be a `Tensor` shaped:
+            `[batch_size, cell_fw.output_size + cell_bw.output_size]`.
     """
-    # In case of Bi-RNN input we need to concatenate outputs of its forward and backward parts
+
     if isinstance(inputs, tuple):
+        # In case of Bi-RNN, concatenate the forward and the backward RNN outputs.
         inputs = tf.concat(inputs, 2)
+
+    if time_major:
+        # (T,B,D) => (B,T,D)
+        inputs = tf.array_ops.transpose(inputs, [1, 0, 2])
 
     inputs_shape = inputs.get_shape()
     sequence_length = inputs_shape[1].value  # the length of sequences processed in the antecedent RNN layer
