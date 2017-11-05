151880f3743f02e62291fe23a8004ba32f80cbb9
Ilya Ivanov <ilya.ivanov.se@gmail.com>
2017-11-06 02:12:22 +0300

    Replace reshape and matmul with tensordot

--- attention.py
+++ attention.py
@@ -62,13 +62,12 @@ def attention(inputs, attention_size, time_major=False, return_alphas=False):
     b_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
     u_omega = tf.Variable(tf.random_normal([attention_size], stddev=0.1))
 
-    # One fully connected layer with non-linear activation for each of the hidden states;
-    #  the shape of `v` is (B*T,A), where A=attention_size
-    v = tf.tanh(tf.matmul(tf.reshape(inputs, [-1, hidden_size]), W_omega) + tf.expand_dims(b_omega, 0))
-    # For each of the B*T hidden states its vector of size A from `v` is reduced with `u` vector
-    vu = tf.matmul(v, tf.expand_dims(u_omega, -1))   # (B*T, 1) shape
-    vu = tf.reshape(vu, tf.shape(inputs)[:2])        # (B,T) shape
-    alphas = tf.nn.softmax(vu)                       # (B,T) shape also
+    # Applying fully connected layer with non-linear activation to each of the B*T timestamps;
+    #  the shape of `v` is (B,T,D)*(D,A)=(B,T,A), where A=attention_size
+    v = tf.tanh(tf.tensordot(inputs, W_omega, axes=1) + b_omega)
+    # For each of the timestamps its vector of size A from `v` is reduced with `u` vector
+    vu = tf.tensordot(v, u_omega, axes=1)   # (B,T) shape
+    alphas = tf.nn.softmax(vu)              # (B,T) shape also
 
     # Output of (Bi-)RNN is reduced with attention vector; the result has (B,D) shape
     output = tf.reduce_sum(inputs * tf.expand_dims(alphas, -1), 1)
